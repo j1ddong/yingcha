1c1045842381147868d071b7e1a46030815fd038
j1ddong <hju191228@gmail.com>
2022-05-19 14:51:22 +0900

    moviesmodel

--- django/movies/models.py
+++ django/movies/models.py
@@ -16,21 +16,21 @@ class Actor(models.Model):
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    release_date = models.DateField
+    release_date = models.DateField()
     description = models.CharField(max_length=300)
-    run_time = models.IntegerField
+    run_time = models.IntegerField()
     country = models.CharField(max_length=30)
     original_title = models.CharField(max_length=100)
-    adult = models.BooleanField
-    poster_url = models.TextField
+    adult = models.BooleanField()
+    poster_url = models.TextField()
     genres = models.ManyToManyField(Genre, related_name='movies')
     directors = models.ManyToManyField(Director, related_name='movies')
     actors = models.ManyToManyField(Actor, related_name='movies')
 
 
 class Review(models.Model):
-    vote_average = models.IntegerField
-    content = models.TextField
+    vote_average = models.IntegerField()
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -39,7 +39,7 @@ class Review(models.Model):
 
 
 class reply(models.Model):
-    content = models.TextField
+    content = models.TextField()
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
