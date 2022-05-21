f80fa9946bde599cfb1700745a274847887f1a8f
jimin <jimin4661@gmail.com>
2022-05-21 17:43:11 +0900

    moviemainview-edit

--- yingcha/src/store/modules/movies.js
+++ yingcha/src/store/modules/movies.js
@@ -1,15 +1,34 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import drf from '@/api/drf'
 
 Vue.use(Vuex)
+const API_KEY = '44f9d36b9d8fa8e880839899c577f866'
+const URL_BASE = 'https://api.themoviedb.org/3'
+const params = {'api_key': API_KEY, 'language': 'ko', 'region': 'KR'}
 
 export default new Vuex.Store({
   state: {
+    boxoffices: [],
+    // popularMovie: [],
+    // bestMovie: [],
+    // genreMovie: [],
   },
   getters: {
+    boxoffices: state => state.boxoffices
   },
   mutations: {
+    GET_BOXOFFICE: (state, boxoffices) => state.boxoffices = boxoffices,
   },
   actions: {
+    fetchBoxoffices(){
+      PATH = '/movie/now_playing'
+      axios.get(URL_BASE + PATH, params=params)
+      .then(res =>{
+        console.log(res)
+        state.boxOfficeMovie = res
+    })
+  },
   },
 })
--- yingcha/src/views/MovieMainView.vue
+++ yingcha/src/views/MovieMainView.vue
@@ -1,50 +1,43 @@
 <template>
   <div>
     <h1>mainpage</h1>
+    <main-carousel></main-carousel>
+    <movie-list-box></movie-list-box>
+    <movie-list-popular></movie-list-popular>
+    <movie-list-best></movie-list-best>
+    <movie-list-genre></movie-list-genre>
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/Navbar.vue'
 import MainCarousel from '@/components/MainCarousel.vue'
 import MovieListBox from '@/components/MovieListBox.vue'
 import MovieListPopular from '@/components/MovieListPopular.vue'
 import MovieListBest from '@/components/MovieListBest.vue'
 import MovieListGenre from '@/components/MovieListGenre.vue'
-import axios from 'axios',
+// import { mapActions, mapGetters } from 'vuex'
 
 
 export default {
   name: 'MovieMain',
   component: {
-    NavBar,
     MainCarousel,
     MovieListBox,
     MovieListPopular,
     MovieListBest,
     MovieListGenre,
   },
-  data() {
-    return {
-      boxOfficeMovie: [],
-      popularMovie: [],
-      bestMovie: [],
-      genreMovie: [],
-    }
+  computed: {
+
+  },
+  methods: {
+
   },
   created() {
-    API_KEY = '44f9d36b9d8fa8e880839899c577f866'
-    URL_BASE = 'https://api.themoviedb.org/3'
-    params = {'api_key': API_KEY, 'language': 'ko', 'region': 'KR'}
-    getBoxoffice: function () {
-      PATH = '/movie/now_playing'
-      axios.get(URL_BASE + PATH, parms=params)
-      .then(res =>{
-        console.log(res)
-      })
+
     }
   }
-}
+
 </script>
 
 <style>
