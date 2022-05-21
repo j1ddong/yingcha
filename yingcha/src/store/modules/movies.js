55113b352cb84bc0e15104b53577549eed71b328
j1ddong <hju191228@gmail.com>
2022-05-21 18:05:47 +0900

    detailv2

--- yingcha/src/store/modules/movies.js
+++ yingcha/src/store/modules/movies.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
+// import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 // import drf from '@/api/drf'
 
 Vue.use(Vuex)
-const API_KEY = '44f9d36b9d8fa8e880839899c577f866'
-const URL_BASE = 'https://api.themoviedb.org/3'
-const params = {'api_key': API_KEY, 'language': 'ko', 'region': 'KR'}
+// const API_KEY = '44f9d36b9d8fa8e880839899c577f866'
+// const URL_BASE = 'https://api.themoviedb.org/3'
+// const params = {'api_key': API_KEY, 'language': 'ko', 'region': 'KR'}
 
 export default new Vuex.Store({
   state: {
@@ -21,14 +21,18 @@ export default new Vuex.Store({
   mutations: {
     GET_BOXOFFICE: (state, boxoffices) => state.boxoffices = boxoffices,
   },
-  actions: {
-    fetchBoxoffices(){
-      PATH = '/movie/now_playing'
-      axios.get(URL_BASE + PATH, params=params)
-      .then(res =>{
-        console.log(res)
-        state.boxOfficeMovie = res
-    })
-  },
-  },
+  // actions: {
+  //   fetchBoxoffices(){
+  //     axios({
+  //       url:
+  //       method:
+  //     })
+  //     const PATH = '/movie/now_playing'
+  //     axios.get(URL_BASE + PATH, params=params)
+  //     .then(res =>{
+  //       console.log(res)
+  //       state.boxOfficeMovie = res
+  //   })
+  // },
+  // },
 })
