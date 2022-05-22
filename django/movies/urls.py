b35742696470adc53ab61f3e57700bb96b5c6d8a
jimin <jimin4661@gmail.com>
2022-05-22 20:52:45 +0900

    django genre algo

--- django/movies/urls.py
+++ django/movies/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     # path('get_1000/', tmdbapi.get_1000),
     # path('get_top_rated/', tmdbapi.get_top_rated),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('director/<int:director_pk>/', views.director, name='director'),
+    path('actor/<int:actor_pk>/', views.actor, name='actor'),
+
 ]
--- django/movies/views.py
+++ django/movies/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Genre, Movie, Director, Actor
+import random
 from pprint import pprint
 from django.shortcuts import render
 from pprint import pprint
-import requests
-from django.http.response import JsonResponse
+from django.core import serializers
+from .serializers import GenreSerializer, MovieSerializer, DirectorSerializer, ActorSerializer
+from rest_framework.response import Response
+from django.http.response import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
 
 
 API_KEY = '44f9d36b9d8fa8e880839899c577f866'
@@ -13,76 +19,61 @@ params = {'api_key': API_KEY, 'language': 'ko', 'region': 'KR'}
 # movie = r1.json()
 
 def index(request):
-    # return_dict = {}
 
-    # #1. tmdb에서 현재 박스오피스 순위 받아오기
-    # PATH1 = '/movie/now_playing'
-    # boxoffice_r = requests.get(URL_BASE + PATH1, params=params)
-    # boxoffice_all = boxoffice_r.json()['results']
-    # boxoffice_selected = []
-    # # pprint(boxoffice_all[:10]) # 객체 형태로 넘어옴
+    # 4. tmdb에서 장르별 영화 받아오기
+    # 1) 장르 리스트에서 id값만 받아온다. --> 가장 보편적인 10개 선정
+    genre_id = [18, 27, 28, 35, 53, 10749, 16, 12, 80, 878]
+    movies_json = []
+    # 2. genreid로 movie에 접근
+    for pk in genre_id:
+        genre = get_object_or_404(Genre, pk=pk)
+        # print(genre)
+        movies = genre.movies.all() # random으로 하나 추출 
+        movie_genre = random.choice(movies)
 
-    # cnt = 0
-    # for movie in boxoffice_all:
-    #     if cnt == 10 :
-    #         break
+        def check(movie_genre, movies_json):
+            while movie_genre in movies_json:
+                movie_genre = random.choce(movies)
+            return movie_genre
+            
 
-    #     boxoffice_selected.append(
-    #         {
-    #             'id' : movie['id'],
-    #             'title' : movie['title'],
-    #             'poster_path' : movie['poster_path'],
-    #             # 'popularity' : movie['popularity']
-    #         }
-    #     )
-    #     cnt += 1
-    # # return JsonResponse(boxoffice_selected, safe=False)
-    # return_dict['boxoffice'] = boxoffice_selected
-
+        # random_one이 result 배열 내에 없으면, result에 넣기
+        chosen_movie = check(movie_genre, movies_json)
     
-    #2. tmdb에서 천만 영화 받아오기
-    korea_popular = [282631, 567646, 397567, 313108, 299534, 330457, 346646, 19995, 1255, 124157, 158445, 291549, 420817, 133200, 45035, 518068, 437068, 11658, 396535, 33196, 242452, 299536, 19644, 99861, 496243, 157336, 109445]
-    popular_selected = []
-    for popular_id in korea_popular:
-        PATH2 = f'/movie/{popular_id}'  # movie.detail
-        popular_r = requests.get(URL_BASE + PATH2, params=params) 
-        popular_movie = popular_r.json()
-        popular_selected.append(
+        movies_json.append(
             {
-                'id': popular_movie['id'],
-                'title': popular_movie['title'],
-                'poster_path': popular_movie['poster_path'],
-
-            }
-        )
-        # pprint(popular_selected)
-    return JsonResponse(popular_selected, safe=False)
+                'id' : chosen_movie.pk,
+                'title': chosen_movie.title,
+                'poster_url': chosen_movie.poster_url,
+                'genre': pk,
+                # 'release_date' : movie_genre.release_date,
+                # 'description': movie_genre.description,
+                # 'run_time': movie_genre.run_time,
+                # 'original_title': movie_genre.original_title,
+                # 'adult': movie_genre.adult,
+                # 'country': movie_genre.country,
+                # 'actor': movie_genre.actor,
+                # 'director': movie_genre.director,
+                # 'provider': movie_genre.provider,
+            })
     
-    #3. tmdb에서 평균 별점이 높은 영화 받아오기
-    # PATH3 = '/movie/top_rated'
-    # top_r = requests.get(URL_BASE + PATH3, params=params)
-    # top_all = top_r.json()['results']
-    # top_selected = []
-    # pprint(top_all[:10]) # 객체 형태로 넘어옴
+    # print(movies_json)
 
-    # cnt2 = 0
-    # for movie in top_all:
-    #     if cnt2 == 10 :
-    #         break
+    return JsonResponse(movies_json, safe=False)
 
-    #     top_selected.append(
-    #         {
-    #             'id' : movie['id'],
-    #             'title' : movie['title'],
-    #             'poster_path' : movie['poster_path'],
-    #             # 'popularity' : movie['popularity']
-    #         }
-    #     )
-    #     cnt2 += 1
-    # return_dict['top'] = top_selected
-    
-    #4. tmdb에서 장르별 영화 받아오기
-    # 1) 장르 리스트에서 id값만 받아온다.
-    # 2) 아이디값을 무작위로 선정한다.
-    # 3) 영화
+
+@api_view(['GET'])
+def director(request, director_pk):
+    director = get_object_or_404(Director, pk=director_pk)
+    serializer = DirectorSerializer(director)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def actor(request, actor_pk):
+    actor = get_object_or_404(Actor, pk=actor_pk)
+    serializer = ActorSerializer(actor)
+    return Response(serializer.data)
+
+
 
