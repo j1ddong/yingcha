67346286ca0377a7001fd49df21f89ffa4e409be
jimin <jimin4661@gmail.com>
2022-05-23 22:25:42 +0900

    foodselect

--- yingcha/src/components/foodcreate/FoodSelect.vue
+++ yingcha/src/components/foodcreate/FoodSelect.vue
@@ -1,40 +1,11 @@
 <template>
-  <div id="nav">
-    <div class="menu-nav">
-      <form action="">
-        <input type="text" v-model="apple" placeholder="검색되는 음식만 선택 가능합니다." 
-        @keyup.space="searchKeyword(apple)" @keyup.enter="searchKeyword(apple)">
-      </form>
-    </div>
-    <ul v-for="keyword in keywords" :key="keyword.id">
-      <li> {{ keyword.title }}</li>
-    </ul>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-
 export default {
-  name: 'FoodSelect',
-  data() {
-    return {
-      apple: '',
-    }
-  },
-  computed: {
-    ...mapGetters(['keywords'])
-  },
-  methods: {
-    ...mapActions(['searchKeyword']),
-    // searchFood () {
-    //   console.log('search food') //이것도 ok
-    //   console.log(this.keyword) // undefined
-    //   this.searchKeyword(this.keyword)
-    // }
-    }
-}
 
+}
 </script>
 
 <style>
