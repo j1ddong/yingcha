0a06741fe49aa84f527151e342da9dbbcffcc415
j1ddong <hju191228@gmail.com>
2022-05-24 15:55:07 +0900

    algo

--- django/communities/admin.py
+++ django/communities/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Food, Article, Comment, BlackList, Taste
+from .models import Food, Article, Comment, Algo, Taste
 # Register your models here.
 admin.site.register(Food)
 admin.site.register(Article)
 admin.site.register(Taste)
 admin.site.register(Comment)
-admin.site.register(BlackList)
+admin.site.register(Algo)
