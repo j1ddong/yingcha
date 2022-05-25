479198cf4dd7af84753cd721fa9bc4b5fa3e09c9
j1ddong <hju191228@gmail.com>
2022-05-25 22:20:49 +0900

    main-page

--- yingcha/src/components/moviemain/MovieListBest.vue
+++ yingcha/src/components/moviemain/MovieListBest.vue
@@ -1,25 +1,59 @@
 <template>
   <div>
-    <h2>평점이 높은 영화</h2>
-    <!-- <p>{{bestmovies}}</p> -->
-    <movie-list-best-items v-for="movie in bestmovies" :key="movie.id" :movie="movie"></movie-list-best-items>
+    <i class="fa-solid fa-angle-left fa-3x arrow-left"></i>
+    <i class="fa-solid fa-angle-right fa-3x arrow-right"></i>
+    <div class="margin">
+      <h2 class="text-left">평점이 높은 영화</h2>
+      <VueSlickCarousel v-bind="settings">
+        <movie-list-best-items v-for="movie in bestmovies" :key="movie.id" :movie="movie"></movie-list-best-items>
+      </VueSlickCarousel>
+    </div>
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import MovieListBestItems from '@/components/moviemain/MovieListBestItems.vue'
 
 export default {
   name: 'MovieListBest',
   components:{
-    MovieListBestItems
+    MovieListBestItems, VueSlickCarousel
   },
   props: {
     bestmovies: Array,
-  }
+  },
+  data () {
+    return {
+      settings: {
+        "dots": false,
+        "focusOnSelect": true,
+        "infinite": true,
+        "speed": 500,
+        "slidesToShow": 5,
+        "initialSlide": 0
+      }
+    }
+  },
 }
 </script>
 
 <style>
-
+.margin {
+  margin: 10px 15rem 0 19rem;
+}
+.arrow-left {
+  position: relative;
+  top: 220px;
+  left: 230px;
+  cursor: pointer;
+}
+.arrow-right {
+  position: relative;
+  top: 220px;
+  left: 1600px;
+  cursor: pointer;
+}
 </style>
--- yingcha/src/components/moviemain/MovieListPopular.vue
+++ yingcha/src/components/moviemain/MovieListPopular.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
-    <h1>천만영화</h1>
-    <!-- <p>{{popularmovies[1]}}</p> -->
-    <movie-list-popular-items v-for="movie in popularmovies" :key="movie.id" :movie="movie"></movie-list-popular-items>
+    <i class="fa-solid fa-angle-left fa-3x arrow-left"></i>
+    <i class="fa-solid fa-angle-right fa-3x arrow-right"></i>
+    <div class="margin">
+      <h1 class="text-left">천만영화</h1>
+      <VueSlickCarousel v-bind="settings">
+        <movie-list-popular-items 
+        v-for="movie in popularmovies" :key="movie.id" :movie="movie"
+        ></movie-list-popular-items>
+      </VueSlickCarousel>
+    </div>
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import MovieListPopularItems from '@/components/moviemain/MovieListPopularItems.vue'
 
 export default {
   name: 'MovieListPopular',
+  data () {
+    return {
+      settings: {
+        "dots": false,
+        "focusOnSelect": true,
+        "infinite": true,
+        "speed": 500,
+        "slidesToShow": 5,
+        "initialSlide": 0,
+      }
+    }
+  },
   components: {
     MovieListPopularItems,
+     VueSlickCarousel
   },
   props: {
     popularmovies: Array,
@@ -21,5 +44,19 @@ export default {
 </script>
 
 <style>
-
+.margin {
+  margin: 10px 15rem 0 19rem;
+}
+.arrow-left {
+  position: relative;
+  top: 220px;
+  left: 230px;
+  cursor: pointer;
+}
+.arrow-right {
+  position: relative;
+  top: 220px;
+  left: 1600px;
+  cursor: pointer;
+}
 </style>
